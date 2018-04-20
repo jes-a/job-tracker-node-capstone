de81f68e5b4cced1e839288e91860f546a037409
Jes Albro <jesalbro@gmail.com>
2018-04-19 17:14:30 -0700

    Update worker profile section with edit form

--- public/client.js
+++ public/client.js
@@ -44,29 +44,9 @@ function showWorkerJobListScreen() {
 
 // ----------- DOCUMENT READY FUNCTION ---------------------
 
-// $(document).ready(function() {
-//     $('#login-screen').show();
-//     $('html').removeClass('white-bg');
-//     $('.js-menu-btn').hide();
-//     $('.js-menu').hide();
-//     $('#admin-home').hide();
-//     $('#add-job-screen').hide();
-//     $('#edit-job-screen').hide();
-//     $('#job-list-screen-admin').hide();
-//     $('#add-worker-screen').hide();
-//     $('#worker-list-screen').hide();
-//     $('#worker-detail-screen').hide();
-//     $('#edit-worker-screen').hide();
-//     $('.js-menu-btn').hide();
-//     $('.js-worker-menu').hide();
-//     $('#job-list-screen-worker').hide();
-//     $('#worker-profile-screen').hide();
-// });
-
-// // for testing purposes
 $(document).ready(function() {
-    $('#login-screen').hide();
-    $('html').addClass('white-bg');
+    $('#login-screen').show();
+    $('html').removeClass('white-bg');
     $('.js-menu-btn').hide();
     $('.js-menu').hide();
     $('#admin-home').hide();
@@ -77,13 +57,33 @@ $(document).ready(function() {
     $('#worker-list-screen').hide();
     $('#worker-detail-screen').hide();
     $('#edit-worker-screen').hide();
-    $('.js-worker-menu-btn').show();
+    $('.js-menu-btn').hide();
     $('.js-worker-menu').hide();
-    $('#job-list-screen-worker').show();
-    $('.js-add-note-section').hide();
+    $('#job-list-screen-worker').hide();
     $('#worker-profile-screen').hide();
-    $('.js-change-pw-section').hide();
 });
+
+// // for testing purposes
+// $(document).ready(function() {
+//     $('#login-screen').hide();
+//     $('html').addClass('white-bg');
+//     $('.js-menu-btn').show();
+//     $('.js-menu').hide();
+//     $('#admin-home').show();
+//     $('#add-job-screen').hide();
+//     $('#edit-job-screen').hide();
+//     $('#job-list-screen-admin').hide();
+//     $('#add-worker-screen').hide();
+//     $('#worker-list-screen').hide();
+//     $('#worker-detail-screen').hide();
+//     $('#edit-worker-screen').hide();
+//     $('.js-worker-menu-btn').hide();
+//     $('.js-worker-menu').hide();
+//     $('#job-list-screen-worker').hide();
+//     $('.js-add-note-section').hide();
+//     $('#worker-profile-screen').hide();
+//     $('.js-change-pw-section').hide();
+// });
 
 // ----------- ADMIN SCREEN TRIGGERS ---------------------
 
@@ -113,7 +113,7 @@ $('#js-login-button').on('click', function(event) {
         })
         .done(function (result) {
             console.log(result.type);
-            if (result.type == 'type-worker') {
+            if (result.type == 'worker') {
                 showWorkerJobListScreen();
             } else {
                 showAdminLandingScreen();
@@ -429,29 +429,34 @@ $('.js-profile-link').on('click', function(event) {
     $('#job-list-screen-worker').hide();
     $('.js-add-note-section').hide();
     $('#worker-profile-screen').show();
-    $('.js-change-pw-section').hide();
+    $('#js-edit-profile-section').hide();
 });
 
 // Open Notes section in Job List when Add Notes + Hours is clicked
-// **** HOW TO TOGGLE OPEN ONLY CLOSEST NOTE SECTION??
 $('.js-add-notes-btn').on('click', function(event) {
     event.preventDefault();
     $(this).parent().find('.js-add-note-section').show();
+    $('textarea').focus();
 });
 
 // Hide Notes section when cancel is clicked
 $('.js-notes-cancel-button').on('click', function(event) {
+    event.preventDefault();
     $('.js-add-note-section').hide();
 })
 
-// Open Change Password section in Profile
-$('.js-change-password-btn').on('click', function(event) {
-    $('.js-change-pw-section').show();
+// Open Edit Profile section in Profile
+$('.js-edit-profile-btn').on('click', function(event) {
+    $('#js-edit-profile-section').show();
+    $('#js-profile').hide();
+    $('#edit-profile-phone-number').focus();
 });
 
-// Hide Change Password section when cancel is clicked
+// Hide Edit Profile section when cancel is clicked
 $('.js-profile-cancel-button').on('click', function(event) {
-    $('.js-change-pw-section').hide();
+    $('#js-edit-profile-section').hide();
+    $('#js-profile').show();
+
 })
 
 
